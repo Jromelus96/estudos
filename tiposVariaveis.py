aaf339fdd46addec0401dc86e4322c2eb0acb70e
Jromelus96 <jromelus96@gmail.cm>
2020-07-29 09:11:22 -0700

    tipos variaveis

---
+++ tiposVariaveis.py
@@ -0,0 +1,11 @@
+#Vou criar 4 variáveis ​​com 4 tipos diferentes (tipos padrão em python)
+
+var1 = 123 # variavel intreira(número inteiro)
+var2 = 123,99 # variavel float(número decimal de ponto flutuante)
+var3 = "700" # variavel string(textual)
+
+#Vou executar uma operação para mostrar a variável boo
+x = 2
+y = 8
+print(f"{var1}\n{var2}\n{var3}")
+print(x == y) # variavel bool(verrdadeiro ou false)
