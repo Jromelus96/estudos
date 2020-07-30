13dfcb9551b452136a1f76724b65ac3d3fb1fc66
Jromelus96 <jromelus96@gmail.cm>
2020-07-30 14:20:59 -0700

    Listas

--- Listas-py.py
+++ Listas-py.py
@@ -5,9 +5,9 @@
 minha_lista1= [1, 2, 3, 4, 5] # uma lista de numeros inteiros
 
 
-#minha lista pode conter qualquer tipo de variável?
+#minha lista pode conter qualquer tipo de variável ?
 
 minha_lista2 = [123, "Abacaxi", "por", 15.99, True] # uma lista com varias tipos de variaveis
 
-git addinha_lista1)
+print(minha_lista1)
 print(minha_lista2)
