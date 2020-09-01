d9f6f51385940d5720b2259b39204c4af4c4a409
Jromelus96 <jromelus96@gmail.com>
2020-09-01 13:31:47 -0700

    Font end

--- frontend1/Projeto calculo IMC/scriptimc.js
+++ frontend1/Projeto calculo IMC/scriptimc.js
@@ -1,14 +1,17 @@
-var nome = prompt("Digite seu nome");
-var altura = prompt("Digite sua altura");
-var peso = prompt("Digite seu peso");
-    imc=peso/(altura*altura);
+function IMC(){
+    var altura = parseInt(document.getElementById("a").value)/100;
+    var peso = parseInt(document.getElementById("p").value);
+    var imc = peso / (altura*altura)
+    var text = "<h1> você esta abaixo do peso</h1>";
+    if(imc >18.5 && imc< 25){
+			text =  " <h1>seu peso esta normal</h1>";}
+	else if (imc > 25 && imc < 30){
+			text = " <h1> Você esta acima do peso </h1> ";
+	}else if (imc>30) {
+			text = " <h1>voce está com Obesidade de fofura</h1> ";
+	}
+	resultado = document.getElementById("result");
+	resultado.innerHTML = text;
 
-		if(imc < 18.5){
-			document.write( nome +  "<h1> você esta abaixo do peso</h1>")
-		}else if(imc >18.5 && imc< 25){
-			document.write( nome +  " <h1>seu peso esta normal</h1>")
-		}else if(imc > 25 && imc < 30){
-			document.write( nome +  " <h1> Voce esta acima do peso </h1> ");
-		}else if (imc>30) {
-			document.write( nome +  " <h1>voce está com Obesidade morbida</h1> ");
-		}
+}
+
