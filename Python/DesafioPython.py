720fc3c463b53de6a0891f1bf0cd94535a3722f7
Jromelus96 <68753987+Jromelus96@users.noreply.github.com>
2020-09-04 07:56:24 -0700

    DesafioPython

--- Python/DesafioPython.py
+++ Python/DesafioPython.py
@@ -7,5 +7,5 @@ with open("C:/Users/Magic Studio/Downloads/mutation.csv", "r") as mutatation:
     for linha in arquivo:
         print(linha, end="")
     quebra = arquivo.split()
-    dic[quebra[0]] = [quebra[1], quebra[2]]
+    dic[quebra[0]] = [quebra[1], quebra[2], quebra[3], quebra[4], quebra[5], quebra[6]]
     print(dic)
