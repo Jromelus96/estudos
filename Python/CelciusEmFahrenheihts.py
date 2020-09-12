caf479f133fd6016a0fb9821d8e9ba800ca470ed
Jromelus96 <68753987+Jromelus96@users.noreply.github.com>
2020-09-12 14:40:05 -0700

    DesafioFuncao02

--- Python/CelciusEmFahrenheihts.py
+++ Python/CelciusEmFahrenheihts.py
@@ -1,21 +1,6 @@
-def meu_function():
+def meu_function(a):
     c = float(input("informe a temperatura °C: "))
     f = ((c * 9) / 5) + 32
     print(" A temperatura de {}°C corresponde a {}°F!".format(c,f))
-
-meu_function()
-
-
-"""Uma função é um bloco de código que só executa quando chamado.
-Você pode passar dados, chamados de parâmetros, para uma função.
-Uma função pode retornar dados de acordo.
-Em Python, uma função é definida usando a palavra-chave def:
-Para chamar uma função, use o nome da função seguido por parênteses:"""
-
-
-#Argumentos ( A estudar melhor)
-"""As informações podem ser passadas para funções como argumentos.
-Os argumentos são especificados após o nome da função, entre parênteses. 
-Você pode adicionar quantos argumentos quiser, apenas separe-os com uma vírgula.
-O exemplo a seguir tem uma função com um argumento (fname). Quando a função é chamada,
- passamos um primeiro nome, que é usado dentro da função para exibir o nome completo:"""
+    print(a)
+meu_function("argumento")
---
+++ Python/DesafioFunction02.py
@@ -0,0 +1,16 @@
+def funcao(x,y):
+    soma = x + y
+    return soma
+print(funcao(2, 3))
+
+
+def adicionar(x):
+    soma = x + 1
+    return soma
+print(adicionar(9))
+
+def converter():
+    m = int(input("entra o tempo em minute: "))
+    s = m * 60
+    return s
+print(converter())
