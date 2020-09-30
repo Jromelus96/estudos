b72e069259c8526600c8c88c54d6ad92246a33ad
unknown <jromelus96@gmail.com>
2020-09-30 08:59:57 -0700

    Query Select Command

---
+++ Python/GenesSelect.py
@@ -0,0 +1,21 @@
+import  psycopg2
+
+conn = psycopg2.connect(f"dbname='Genes' "
+                        f"user='postgres' "
+                        f"host='localhost'"
+                        f"password='Mcherving18'")
+
+cur = conn.cursor()
+
+# Query_Select
+query = "select id,name from dados"
+cur.execute(query)
+print("Os itens a selecionar são")
+resultado = cur.fetchall()
+print(resultado)
+
+
+if(conn):
+    cur.close()
+    conn.close()
+    print("Postgres esta fechado")
